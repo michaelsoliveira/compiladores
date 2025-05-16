c9e53a27a96e100de237bbfd003f153cef270e63
Michael Oliveira <michaelsoliveira@gmail.com>
2025-05-16 15:11:39 -0300

    Atualizações

--- src/GeneratorC.java
+++ src/GeneratorC.java
@@ -5,6 +5,11 @@ import java.io.File;
 public class GeneratorC {
     public static void main(String[] args) {
         File sourceCode = new File(new File("").getAbsolutePath() + "\\c.lex");
-        jflex.Main.generate(sourceCode);
+        try {
+            jflex.Main.generate(new String[] { sourceCode.getPath() });
+        } catch (Exception e) {
+            System.err.println("Erro ao gerar analisador léxico com JFlex: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
